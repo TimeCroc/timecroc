8a07f946ec513b20e72e06358ddc5e0462741839
Clare Cerullo <jewelofsong@gmail.com>
2023-03-24 20:01:53 -0400

    implementing logic for bcrypt and jwt  in sessionController
    Co-authored-by: Kat Athay <KatAthay@users.noreply.github.com>
    Co-authored-by: Mark Charles Smith <markcharlessmith@users.noreply.github.com>

--- server/Controllers/sessionController.js
+++ server/Controllers/sessionController.js
@@ -1,39 +1,176 @@
-const bcrypt = require("bcrypt");
-const path = require('path');
-const db = require(path.resolve(__dirname, '../models/adminModel'));
+// const bcrypt = require("bcrypt");
+// const path = require('path');
+// const db = require(path.resolve(__dirname, '../models/adminModel'));
+// const jwt = require("jsonwebtoken");
+
+// const sessionController = {};
+
+// sessionController.verifyAdmin = async (req, res) => {
+//   const { email, admin_password } = req.body;
+//   console.log("email: ", email, "password: ", admin_password);
+//   try {
+//     const data = await db.query(`SELECT * FROM admin WHERE email= $1;`, [email]);
+//     const admin = data.rows;
+//     if (admin.length === 0) {
+//       res.status(400).json({error: "Admin is not registered."});
+//     }
+//     else {
+//       bcrypt.compare(admin_password, admin[0].admin_password, (err, result) => {
+//         if (err) {
+//           res.status(500).json({error: "Server error."});
+//         }
+//         else if (result === true) {
+//           const token = jwt.sign({email: email}, process.env.SECRET_KEY);
+//           res.status(200).json({message: "Admin signed in!", token: token});
+//         }
+//         else {
+//           if (result !== true) {
+//             res.status(400).json({error: "Enter correct password."})
+//           }
+//         }
+//       })
+//     }
+//   }
+//   catch (err) {
+//     console.log(err);
+//     res.status(500).json({error: "Database error occurred while signing in"});
+//   }
+// }
+
+// module.exports = sessionController;
+
+// second attempt
+// const bcrypt = require("bcrypt");
+// const path = require('path');
+// const db = require(path.resolve(__dirname, '../models/adminModel'));
+// const jwt = require("jsonwebtoken");
+
+// const sessionController = {};
+// const secretKey = process.env.SECRET_KEY;
+
+// sessionController.verifyAdmin = async (req, res, next) => {
+//   const token = req.headers.authorization?.split(' ')[1]; // get JWT token from request headers
+//   console.log(token)
+//   if (!token) {
+//     return res.status(401).json({ error: 'No token provided.' });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, secretKey); // decode JWT token
+//     const { email } = decoded;
+
+//     const data = await db.query(`SELECT * FROM admin WHERE email= $1;`, [email]);
+//     const admin = data.rows;
+//     if (admin.length === 0) {
+//       return res.status(400).json({ error: "Admin is not registered." });
+//     }
+
+//     // add admin object to request for use in other middleware
+//     req.admin = admin[0];
+
+//     next(); // proceed to next middleware
+//   } catch (err) {
+//     console.log(err);
+//     res.status(401).json({ error: 'Invalid token.' });
+//   }
+// };
+
+// module.exports = sessionController;
+
+// third attempt
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const path = require("path");
+const db = require(path.resolve(__dirname, "../models/adminModel")); // assuming you have a Admin model defined
 
 const sessionController = {};
 
-sessionController.verifyAdmin = async (req, res) => {
+sessionController.createSession = async (req, res) => {
   const { email, admin_password } = req.body;
+
+  // find user by email
+  const admin = await db.query("SELECT * FROM admin WHERE email = $1", [email]);
+  console.log("admin", admin);
+  if (!admin.rows[0]) {
+    return res.status(401).json({ message: "Invalid email" });
+  }
+
+  const inputPassword = await admin.rows[0].admin_password;
+  console.log("inputPassword", inputPassword);
+  console.log('admin_password', admin_password)
+  // check password
+  // note: bcrypt.compare returns a promise, and it requires three arguments, the third of which is a callback function that will be called when the promise is resolved
+  bcrypt.hash(admin_password, saltRounds, function(err, hash) {
+    // Store hash in your password DB
+  });
+
+  const validPassword = await bcrypt.compare(admin_password, inputPassword);
+  console.log(validPassword, 'validPassword');
+  if (!validPassword) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+
+  // create JWT
+  const token = jwt.sign(
+    { email: admin.rows[0].email },
+    process.env.JWT_SECRET
+  );
+
+  // specify cookie options for production versus development
+  const cookieOptions = {
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    secure: process.env.NODE_ENV === "production", // true in production, false otherwise
+    httpOnly: process.env.NODE_ENV === "production", // true in production, false otherwise
+    sameSite: "strict",
+  };
+
+  if (process.env.NODE_ENV !== "production") {
+    // Disable httpOnly flag in non-production environment
+    cookieOptions.httpOnly = false;
+  }
+
+  // set cookie
+  res.cookie("session_id", token, cookieOptions);
+
+  res.json({ message: "Session created" });
+};
+
+sessionController.verifySession = async (req, res, next) => {
+  const token = req.cookies.token; //replace token with session_id
+  if (!token) {
+    return res.status(401).json({ message: "Token not valid" });
+  }
+
   try {
-    const data = await db.query(`SELECT * FROM admin WHERE email= $1;`, [email]);
-    const admin = data.rows;
-    if (admin.length === 0) {
-      res.status(400).json({error: "Admin is not registered."});
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const admin = await db.query("SELECT * FROM admin WHERE email = $1", [
+      decoded.email,
+    ]);
+    if (!admin) {
+      return res.status(401).json({ message: "Invalid admin" });
     }
-    else {
-      bcrypt.compare(admin_password, admin[0].admin_password, (err, result) => {
-        if (err) {
-          res.status(500).json({error: "Server error."});
-        }
-        else if (result === true) {
-          const token = jwt.sign({email: email}, process.env.SECRET_KEY);
-          res.status(200).json({message: "Admin signed in!", token: token});
-        }
-        else {
-          if (result !== true) {
-            res.status(400).json({error: "Enter correct password."})
-          }
-        }
-      })
-    }
+
+    req.admin = admin;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unexpected error" });
   }
-  catch (err) {
-    console.log(err);
-    res.status(500).json({error: "Database error occurred while signing in"});
-  }
-}
+};
 
 module.exports = sessionController;
+
+
+/* 
+const validPassword = await bcrypt.compare(admin_password, admin.rows[0].admin_password, (err, res) => {
+  if (err) {
+    throw err;
+  }
+  return res;
+});
+
+if (!validPassword) {
+  return res.status(401).json({ message: "Invalid password" });
+}
+
+*/
--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -4,14 +4,14 @@ const adminRouter = express.Router();
 const adminController = require('../Controllers/adminController');
 const sessionController = require('../Controllers/sessionController');
 
-adminRouter.get('/', sessionController.verifyAdmin, adminController.getAllAdmins, (req, res) => { return res.status(200).json(res.locals.admins); });
+adminRouter.get('/', sessionController.createSession, sessionController.verifySession, adminController.getAllAdmins, (req, res) => { return res.status(200).json(res.locals.admins); });
 
-adminRouter.get('/:id', sessionController.verifyAdmin, adminController.getAdminById, (req, res) => { return res.status(200).json(res.locals.targetAdmin); });
+adminRouter.get('/:id', sessionController.createSession, sessionController.verifySession, adminController.getAdminById, (req, res) => { return res.status(200).json(res.locals.targetAdmin); });
 
-adminRouter.post('/', sessionController.verifyAdmin, adminController.createAdmin, (req, res) => { return res.status(200).json(res.locals.newAdmin); });
+adminRouter.post('/', sessionController.createSession, sessionController.verifySession, adminController.createAdmin, (req, res) => { return res.status(200).json(res.locals.newAdmin); });
 
-adminRouter.put('/:id', sessionController.verifyAdmin, adminController.updateAdmin, (req, res) => { return res.status(200).json(res.locals.updatedAdmin); });
+adminRouter.put('/:id', sessionController.createSession, sessionController.verifySession, adminController.updateAdmin, (req, res) => { return res.status(200).json(res.locals.updatedAdmin); });
 
-adminRouter.delete('/:id', sessionController.verifyAdmin, adminController.deleteAdmin, (req, res) => { return res.status(200).json(res.locals.deletedAdmin); });
+adminRouter.delete('/:id', sessionController.createSession, sessionController.verifySession, adminController.deleteAdmin, (req, res) => { return res.status(200).json(res.locals.deletedAdmin); });
 
 module.exports = adminRouter;
