b3a516b629c224a4077f089029cdbc6767cb65d3
Mark Charles Smith <markcharlessmith@gmail.com>
2023-04-20 04:14:57 -0700

    slight changes to index.js and webpack.config.js for bugfix
    Co-authored-by: CLARECERULLO <JEWELOFSONG@GMAIL.COM>
    Co-authored-by: KATATHAY <KODERKAT@OUTLOOK.COM>

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/styles.css";
