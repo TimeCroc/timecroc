8dd044f934b4f31de435ff810469ba76488bfbd9
Mark Charles Smith <markcharlessmith@gmail.com>
2023-05-02 07:58:35 -0700

    added functionalty requested for Validation.js in TC-21

--- client/components/Validation.js
+++ client/components/Validation.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+// added imports for useNavigate and Button
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Validation = (props) => {
   const { validationMessage, startTime, endTime } = props;
+  const navigate = useNavigate();
 
-  if(endTime){
-    return (
-      <div className='validation'>
-        <h3>{validationMessage}{endTime}</h3>
-        <Link to='/'>Exit</Link>
-      </div>
-    )
-  }
+  // Definition of handleTimeout function that navigates to the home page
+  const handleTimeout = () => {
+    navigate('/');
+  };
 
+  // Utilize useEffect to set a timeout for 3 seconds that calls the handleTimeout function
+  useEffect(() => {
+    const timeout = setTimeout(handleTimeout, 3000);
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+
+  // display validationMessage and either startTime or endTime, depending on which is defined
   return (
     <div className='validation'>
-      <h3>{validationMessage}{startTime}</h3>
-      <Link to='/'>Exit</Link>
+      <h3>{validationMessage} {startTime || endTime}</h3>
+      {/* When the button is clicked, call navigate function and direct back to '/' */}
+      <Button className='button' onClick={() => navigate('/')}>Exit</Button>
     </div>
-  )
-
+  );
 }
 
 export default Validation;
