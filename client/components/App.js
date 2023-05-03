2f20d4496b9b2936af0986d3f014e1662843d7aa
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2023-05-03 11:25:13 -0600

    AdminLogIn btn
    Admin login button renders upon employee logging out.

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, Outlet, Link, useLocation } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useLocation,
+  useMatches,
+} from 'react-router-dom';
 import Clock from './Clock';
 import PinPad from './pinpad/PinPad';
 import EmployeeList from './admin/EmployeeList';
@@ -10,7 +17,6 @@ import Timesheet from './Timesheet';
 import Validation from './Validation';
 import NumberPad from './pinpad/NumberPad';
 import logo from '../Rectangle Logo.png';
-
 
 // when we refactor App.js, we should clean up imports
 import AdminLogIn from './admin/AdminLogIn';
@@ -32,115 +38,142 @@ const App = () => {
   const [tours, setTours] = useState(0);
   const [reimbursements, setReimbursements] = useState(0);
   const [DOC, setDOC] = useState(0);
-  
-  // state for admin logged in
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
-  
-  function getStart(num){
+  const isLoggedIn = currentEmployee !== null;
+
+  function getStart(num) {
     let start = parseInt(num);
     let shiftStart = new Date(start);
     let string = shiftStart.toLocaleString();
     setStartTime(string);
-  };
+  }
 
-  function getEnd(num){
+  function getEnd(num) {
     let end = parseInt(num);
     let shiftEnd = new Date(end);
     let string = shiftEnd.toLocaleString();
     setEndTime(string);
-  };
+  }
 
- // useLocation hook to get the current pathname
- const location = useLocation();
- const isAdminPage = location.pathname.startsWith('/admin');
+  // useLocation hook to get the current pathname
+  const location = useLocation();
+  const isAdminPage = location.pathname.startsWith('/admin');
+  
 
- return (
-  <div className='body'>
-    <div className="app-display">
-      {/* Render sign out button only if admin is logged in */}
-      {isAdminLoggedIn && (
-        <Link to="admin/logout"><button className='login-btn'>Log Out</button></Link>
+  return (
+    <div className='body'>
+      <div className='app-display'>
+        {/* Render sign out button only if admin is logged in */}
+        {isAdminLoggedIn && (
+          <Link to='admin/logout'>
+            <button className='login-btn'>Log Out</button>
+          </Link>
+        )}
+
+         {!isAdminLoggedIn && location.pathname === '/' && (
+        <Link to='admin/login'>
+          <button className='login-btn'>Admin Log in</button>
+        </Link>
       )}
 
-      <img src={logo} />
-      <Clock />
+        <img src={logo} />
+        <Clock />
+      </div>
+     
+      {/* Render login button only if not on an admin page and not logged in as admin */}
+      {!isAdminPage && !isAdminLoggedIn && currentEmployee === null && (
+        <Link to='admin/login'>
+          <button className='login-btn'>Admin Log in</button>
+        </Link>
+      )}
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <PinPad
+              setEmployeePin={setEmployeePin}
+              setCurrentEmployee={setCurrentEmployee}
+              setCurrentShift={setCurrentShift}
+              setTips={setTips}
+              setTours={setTours}
+              setReimbursements={setReimbursements}
+              setDOC={setDOC}
+            />
+          }
+        />
+        
+
+        {isAdminLoggedIn ? (
+          <Route
+            path='admin'
+            element={<AdminDashboard isAdminLoggedIn={isAdminLoggedIn} />}
+          />
+        ) : null}
+        <Route
+          path='admin/login'
+          element={
+            <AdminLogIn
+              isAdminLoggedIn={isAdminLoggedIn}
+              setIsAdminLoggedIn={setIsAdminLoggedIn}
+            />
+          }
+        />
+        <Route path='admin/currentPayPeriod' element={<AddEmployee />} />
+        <Route path='admin/previousPayPeriods' element={<EmployeeList />} />
+        <Route path='admin/add' element={<AddEmployee />} />
+        <Route path='admin/list' element={<EmployeeList />} />
+
+        <Route
+          path='employeeportal'
+          element={
+            <EmployeePortal
+              currentEmployee={currentEmployee}
+              currentShift={currentShift}
+              employeePin={employeePin}
+              endTime={currentShift.end_time}
+              startTime={startTime}
+              setTimesheet={setTimesheet}
+              setValidationMessage={setValidationMessage}
+              setEndTime={setEndTime}
+              setStartTime={setStartTime}
+              getStart={getStart}
+              getEnd={getEnd}
+              extrasView={extrasView} //needed?
+              setExtrasView={setExtrasView} //needed?
+              tips={tips}
+              setTips={setTips}
+              tours={tours}
+              setTours={setTours}
+              reimbursements={reimbursements}
+              setReimbursements={setReimbursements}
+              DOC={DOC}
+              setDOC={setDOC}
+            />
+          }
+        />
+        <Route
+          path='employeeportal/timesheet'
+          element={<Timesheet timesheet={timesheet} />}
+        />
+        <Route
+          path='employeeportal/validation'
+          element={
+            <Validation
+              validationMessage={validationMessage}
+              startTime={startTime}
+              endTime={endTime}
+            />
+          }
+        />
+        <Route
+          path='employeeportal/addtips'
+          element={
+            <NumberPad value={tips} setValue={setTips} buttonText='Add Tip' />
+          }
+        />
+      </Routes>
     </div>
-
-    {/* Render login button only if not on an admin page and not logged in as admin */}
-    {!isAdminPage && !isAdminLoggedIn && currentEmployee === null && (
-      <Link to="admin/login"><button className='login-btn'>Admin Log in</button></Link>
-    )}
-
-    <Routes>
-      <Route path="/" 
-        element={<PinPad 
-          setEmployeePin={setEmployeePin} 
-          setCurrentEmployee={setCurrentEmployee}
-          setCurrentShift={setCurrentShift}
-          setTips={setTips}
-          setTours={setTours}
-          setReimbursements={setReimbursements}
-          setDOC={setDOC}
-        />} 
-      />
-
-      {isAdminLoggedIn ? <Route path="admin" element={<AdminDashboard isAdminLoggedIn={isAdminLoggedIn} />} /> : null}
-      <Route path="admin/login" element={<AdminLogIn 
-        isAdminLoggedIn={isAdminLoggedIn}
-        setIsAdminLoggedIn={setIsAdminLoggedIn}
-      />} />
-      <Route path="admin/currentPayPeriod" element={<AddEmployee />} />
-      <Route path="admin/previousPayPeriods" element={<EmployeeList />} />
-      <Route path="admin/add" element={<AddEmployee />} />
-      <Route path="admin/list" element={<EmployeeList />} />
-
-      <Route path="employeeportal" 
-        element={<EmployeePortal 
-          currentEmployee={currentEmployee} 
-          currentShift={currentShift}
-          employeePin={employeePin}
-          endTime={currentShift.end_time}
-          startTime={startTime}
-          setTimesheet={setTimesheet}
-          setValidationMessage={setValidationMessage}
-          setEndTime={setEndTime}
-          setStartTime={setStartTime}
-          getStart={getStart}
-          getEnd={getEnd}
-          extrasView={extrasView} //needed?
-          setExtrasView={setExtrasView} //needed?
-          tips={tips}
-          setTips={setTips}
-          tours={tours}
-          setTours={setTours}
-          reimbursements={reimbursements}
-          setReimbursements={setReimbursements}
-          DOC={DOC}
-          setDOC={setDOC}
-        />} 
-      />
-      <Route path="employeeportal/timesheet" element={<Timesheet timesheet={timesheet}/>}/>  
-      <Route path="employeeportal/validation" 
-        element={<Validation 
-          validationMessage={validationMessage} 
-          startTime={startTime} 
-          endTime={endTime}
-        />}
-      />
-      <Route path="employeeportal/addtips" 
-        element={<NumberPad 
-          value={tips}
-          setValue={setTips}
-          buttonText="Add Tip"
-        />}
-      />
-    </Routes>
-  </div>
-);
-
-}
+  );
+};
 
 export default App;
-
-
-
