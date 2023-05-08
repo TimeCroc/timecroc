eb93c0859ebb03e17b3237ceda464da711e494b0
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2023-05-08 14:48:47 -0600

    fixed pinpad display

--- client/components/ClockInPortal.js
+++ client/components/ClockInPortal.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserClock } from '@fortawesome/free-solid-svg-icons';
+import '../css/styles.css'
 
 const ClockInPortal = (props) => {
   const navigate = useNavigate();
@@ -12,7 +17,8 @@ const ClockInPortal = (props) => {
         <Card.Body>
           <Card.Title>Welcome to work {first_name}</Card.Title>
           <Card.Text> It's time to clock in for your shift! </Card.Text> 
-            <Button className='button' variant="primary" onClick={()=> handleClockIn()}>Clock In</Button>
+            <Button className='button' variant="primary" onClick={()=> handleClockIn()}><FontAwesomeIcon icon={faUserClock} className="white-icon" size="lg"/>   Clock In
+            </Button>
             <Button className='button' variant="secondary" onClick={viewTimesheet}>View Timesheet</Button>
             <Button 
               className='button' 
