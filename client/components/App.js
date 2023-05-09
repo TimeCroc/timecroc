c9ecc9cd0e2f2bef8b2ed199f2348c5ebbfb4179
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2023-05-09 08:57:06 -0600

    fixed app.js routes for setting tips

--- client/components/App.js
+++ client/components/App.js
@@ -11,6 +11,7 @@ import Validation from './Validation';
 import NumberPad from './pinpad/NumberPad';
 import logo from '../Rectangle Logo.png';
 
+
 // when we refactor App.js, we should clean up imports
 import AdminLogIn from './admin/AdminLogIn';
 
@@ -31,22 +32,23 @@ const App = () => {
   const [tours, setTours] = useState(0);
   const [reimbursements, setReimbursements] = useState(0);
   const [DOC, setDOC] = useState(0);
+  
+  // state for admin logged in
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
-
 
   function getStart(num){
     let start = parseInt(num);
     let shiftStart = new Date(start);
     let string = shiftStart.toLocaleString();
     setStartTime(string);
-  }
+  };
 
-  function getEnd(num) {
+  function getEnd(num){
     let end = parseInt(num);
     let shiftEnd = new Date(end);
     let string = shiftEnd.toLocaleString();
     setEndTime(string);
-  }
+  };
 
  // useLocation hook to get the current pathname
  const location = useLocation();
@@ -91,49 +93,35 @@ const App = () => {
         <img src={logo} />
         <Clock />
       </div>
-     
-      {/* Render login button only if not on an admin page and not logged in as admin */}
-      {!isAdminPage && !isAdminLoggedIn && currentEmployee === null && (
-        <Link to='admin/login'>
-          <button className='login-btn'>Admin Log in</button>
-        </Link>
-      )}
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <PinPad
-              setEmployeePin={setEmployeePin}
-              setCurrentEmployee={setCurrentEmployee}
-              setCurrentShift={setCurrentShift}
-              setTips={setTips}
-              setTours={setTours}
-              setReimbursements={setReimbursements}
-              setDOC={setDOC}
-            />
-          }
-        />
-        
-
-        {isAdminLoggedIn ? (
-          <Route
-            path='admin'
-            element={<AdminDashboard isAdminLoggedIn={isAdminLoggedIn} />}
+   
+        <Routes>
+          <Route path="/" 
+            element={<PinPad 
+            setEmployeePin={setEmployeePin} 
+            setCurrentEmployee={setCurrentEmployee}
+            setCurrentShift={setCurrentShift}
+            setTips={setTips}
+            setTours={setTours}
+            setReimbursements={setReimbursements}
+            setDOC={setDOC}
+            />} 
           />
-        ) : null}
-        <Route
-          path='admin/login'
-          element={
-            <AdminLogIn
-              isAdminLoggedIn={isAdminLoggedIn}
-              setIsAdminLoggedIn={setIsAdminLoggedIn}
-            />
-          }
-        />
-        <Route path='admin/currentPayPeriod' element={<AddEmployee />} />
-        <Route path='admin/previousPayPeriods' element={<EmployeeList />} />
-        <Route path='admin/add' element={<AddEmployee />} />
-        <Route path='admin/list' element={<EmployeeList />} />
+          
+          {/* <Route path="admin" element={isAdminLoggedIn ? <AdminDashboard isAdminLoggedIn={isAdminLoggedIn} /> : <AdminLogIn />} /> */}
+          {isAdminLoggedIn ? <Route path="admin" element={<AdminDashboard isAdminLoggedIn={isAdminLoggedIn} />} /> : null}
+              {/* conditional render statement
+                // if the admin is logged in,
+                  // check if the route path is /admin, if so, render the dashboard and sign out button
+                  // if the route path is NOT /admin (e.g. /), 
+               */}
+          <Route path="admin/login" element={<AdminLogIn 
+            isAdminLoggedIn={isAdminLoggedIn}
+            setIsAdminLoggedIn={setIsAdminLoggedIn}
+            />} />
+          <Route path="admin/currentPayPeriod" element={<AddEmployee />} />
+          <Route path="admin/previousPayPeriods" element={<EmployeeList />} />
+          <Route path="admin/add" element={<AddEmployee />} />
+          <Route path="admin/list" element={<EmployeeList />} />
 
 
           <Route path="employeeportal" 
@@ -160,29 +148,52 @@ const App = () => {
               setReimbursements={setReimbursements}
               DOC={DOC}
               setDOC={setDOC}
-
-
             />} 
           />
-          <Route path="employeeportal/timesheet" element={<Timesheet timesheet={timesheet}/>}/>  
+          <Route path="employeeportal/timesheet" element={<Timesheet timesheet={timesheet} currentEmployee={currentEmployee}/>}/>  
           <Route path="employeeportal/validation" 
             element={<Validation
               setCurrentEmployee={setCurrentEmployee} 
               validationMessage={validationMessage} 
               startTime={startTime} 
               endTime={endTime}
-            />
-          }
-        />
-        <Route
-          path='employeeportal/addtips'
-          element={
-            <NumberPad value={tips} setValue={setTips} buttonText='Add Tip' />
-          }
-        />
-      </Routes>
-    </div>
-  );
-};
+            />}
+          />
+          <Route path="employeeportal/addtips" 
+            element={<NumberPad 
+              view={extrasView}
+              setExtrasView={setExtrasView}
+              number={tips}
+              setNumber={setTips}
+            />}
+          />
+          <Route path="employeeportal/addreimbursements" 
+            element={<NumberPad 
+              view={extrasView}
+              setExtrasView={setExtrasView}
+              number={reimbursements}
+              setNumber={setReimbursements}
+            />}
+          />
+          <Route path="employeeportal/addtours" 
+            element={<NumberPad 
+              view={extrasView}
+              setExtrasView={setExtrasView}
+              number={tours}
+              setNumber={setTours}
+            />}
+          />
+          <Route path="employeeportal/adddoc" 
+            element={<NumberPad 
+              view={extrasView}
+              setExtrasView={setExtrasView}
+              number={DOC}
+              setNumber={setDOC}
+            />}
+          />
+        </Routes>  
+      </div>
+    );
+}
 
 export default App;
