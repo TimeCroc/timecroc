d36f9f849a99ad9b3101ecc8e833a69877b08adf
Clare Cerullo <jewelofsong@gmail.com>
2023-05-30 18:41:46 -0400

    refactored middleware in payPeriodController.js and routes in payperiodRoutes.js

--- server/Controllers/payPeriodController.js
+++ server/Controllers/payPeriodController.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const db = require(path.resolve(__dirname, '../models/employeeModel'));
+const ppdb = require(path.resolve(__dirname, '../models/payPeriodModel'));
 
 const payPeriodController = {};
 
@@ -30,8 +31,8 @@ payPeriodController.getOneFromPayPeriodTable = async (req, res, next) => {
     //not sure why this doesn't throw and error automatically?
     if (res.locals.targetPayPeriod === undefined) {
       return next({
-        log: 'no employee found with that id',
-        message: {err: 'no employee found with that id'}
+        log: 'no pay period found with that id',
+        message: {err: 'no pay period found with that id'}
       });
     }
     return next();
@@ -45,61 +46,73 @@ payPeriodController.getOneFromPayPeriodTable = async (req, res, next) => {
 } 
 
 payPeriodController.updatePayPeriodTable = async (req, res, next) => {
-  const { _id } = res.locals.targetPayPeriod;
+  const { _id } = req.params;
   const { payPeriod, employeeId, hours, minutes, tips, reimbursements, tours, doc } = req.body;
 
   try {
     if (_id) {
-      const input = [_id, payPeriod, employeeId, hours, minutes, tips, reimbursements, tours, doc];
+      const input = [employeeId, hours, minutes, tips, reimbursements, tours, doc, _id];
       const query = 'UPDATE payperiods SET \
-      total_hours = $2, total_minutes = $3, total_tips = $4, total_reimbursements = $5, total_tours = $6, total_doc = $7';
-      const updated = await db.query(employeeQuery, input);
+        employee_id = $1, total_hours = $2, total_minutes = $3, \
+        total_tips = $4, total_reimbursements = $5, total_tours = $6, total_doc = $7 \
+        WHERE _id = $8';
+      const updated = await db.query(query, input);
       res.locals.updatedPayPeriod = updated.rows[0];
       return next();
+    } else {
+      const employeeQuery = 'SELECT _id, first_name, last_name FROM employee WHERE _id = $1';
+      const employeeResult = await db.query(employeeQuery, [employeeId]);
+      const employee = employeeResult.rows[0];
+
+      const input = [
+        payPeriod,
+        employeeId,
+        employee.first_name,
+        employee.last_name,
+        hours,
+        minutes,
+        tips,
+        reimbursements,
+        tours,
+        doc
+      ];
+      const query =
+        'INSERT INTO payperiods (\
+          pay_period, employee_id, first_name, last_name, total_hours, \
+          total_minutes, total_tips, total_reimbursements, total_tours, total_doc \
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)\
+        RETURNING *';
+      const newPayPeriod = await ppdb.query(query, input);
+      res.locals.newPayPeriod = newPayPeriod.rows[0];
+      return next();
     }
-  }
-  catch(err) {
+  } catch (err) {
     return next({
       log: 'payPeriodController.updatePayPeriodTable: ERROR',
-      message: {err: 'Error occurred in payPeriodController.updatePayPeriodTable'}
+      message: { err: err.message }
     });
-  };
-}
+  }
+};
 
-// payPeriodController.insertTotalsToDatabase = async (db, payPeriod, employeeId, hours, minutes, tips, reimbursements, tours, doc) => {
-//   try {
-//     // Construct the SQL INSERT ... ON CONFLICT ... DO UPDATE query
-//     const query = `
-//       INSERT INTO payperiods (pay_period, employee_id, first_name, last_name, total_hours, total_minutes, total_tips, total_reimbursements, total_tours, total_doc)
-//       VALUES (pay_period, ?, (SELECT first_name FROM employee WHERE _id = ?), (SELECT last_name FROM employee WHERE _id = ?), ?, ?, ?, ?, ?, ?)
-//       ON CONFLICT (employee_id) DO UPDATE SET
-//         total_hours = EXCLUDED.total_hours,
-//         total_minutes = EXCLUDED.total_minutes,
-//         total_tips = EXCLUDED.total_tips,
-//         total_reimbursements = EXCLUDED.total_reimbursements,
-//         total_tours = EXCLUDED.total_tours,
-//         total_doc = EXCLUDED.total_doc
-//     `;
-    
-//     // Execute the query with the provided values
-//     await db.execute(db, [employeeId, employeeId, employeeId, hours, minutes, tips, reimbursements, tours, doc]);
-
-//     console.log('Totals inserted or updated successfully in the payperiods table.');
-//   } catch (error) {
-//     console.error('Error inserting or updating totals in the payperiods table:', error);
-//   }
-// };
-
-// payPeriodController.databaseMiddleware = (db) => {
-//   return async (req, res, next) => {
-//     const { payPeriod, employeeId, hours, minutes, tips, reimbursements, tours, doc } = req.body;
-    
-//     // Call the function to insert or update totals in the payperiods table
-//     await insertTotalsToDatabase(db, payPeriod, employeeId, hours, minutes, tips, reimbursements, tours, doc);
-
-//     // Continue to the next middleware or route handler
-//     next();
-//   };
-// };
+payPeriodController.deletePayPeriod = async (req, res, next) => {
+  const { _id } = res.locals.targetPayPeriod;
+  try {
+    const input = [];
+    let query = 'DELETE FROM payperiods ';
+    if(_id){
+      query += `WHERE _id = $1 RETURNING * `;
+      input.push(_id);
+    }
+    const deleted = await db.query(query, input);
+    res.locals.deleted = deleted.rows[0];
+    return next();
+  }
+  catch(err){
+    return next({
+      log: 'payPeriodController.deletePayPeriod: ERROR',
+      message: {err: 'Error occurred in payPeriodController.deletePayPeriod'}
+    });
+  }
+};
 
 module.exports = payPeriodController;
--- server/Routes/payPeriodRoutes.js
+++ server/Routes/payPeriodRoutes.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const payPeriodRouter = express.Router();
-const path = require('path');
 const payPeriodController = require('../Controllers/payPeriodController');
-const db = require(path.resolve(__dirname, '../models/employeeModel'));
-
 
 // route for getting all payperiod data in table
 payPeriodRouter.get('/', payPeriodController.getAllPayPeriodData, (req, res) => {
   return res.status(200).json(res.locals.payperiod);
 });
 
-// payPeriodRouter.post('/update', payPeriodController.updatePayPeriodTable, (req, res) => {
-//   return res.status(200);
-// });
+payPeriodRouter.put('/update', payPeriodController.updatePayPeriodTable, (req, res) => {
+  // Return the updated or new pay period data
+  if (res.locals.updatedPayPeriod) {
+    res.json({ updatedPayPeriod: res.locals.updatedPayPeriod });
+  } else if (res.locals.newPayPeriod) {
+    res.json({ newPayPeriod: res.locals.newPayPeriod });
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+payPeriodRouter.delete('/:id', payPeriodController.getOneFromPayPeriodTable, payPeriodController.deletePayPeriod, (req, res) => {
+  return res.status(200).json(res.locals.deleted);
+});
 
 // route for currentPayPeriod
 
