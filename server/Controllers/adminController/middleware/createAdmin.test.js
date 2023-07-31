c5b1dd84419ef3ea000586b9bb28bf55a2f2f9eb
Mark Charles Smith <markcharlessmith@gmail.com>
2023-07-31 08:47:12 -0700

    added tests for createAdmin middleware
    Co-authored-by: CLARECERULLO <JEWELOFSONG@GMAIL.COM>

---
+++ server/Controllers/adminController/middleware/createAdmin.test.js
@@ -0,0 +1,60 @@
+
+// need mock query
+// need to test that newAdmin is created
+// need to test that next() is called
+// need to test that error is thrown if db query fails
+// need to delete dummy data from db after test
+
+const request = require('supertest');
+
+const db = require('../../../models/adminModel');
+
+// unit under test
+const createAdmin = require('./createAdmin');
+
+jest.mock('dotenv');
+jest.mock('../../../models/adminModel', () => ({
+  query: jest.fn()
+}));
+
+describe('createAdmin middleware', () => {
+  it('should create a new admin and call next()', async () => {
+    const mockAdminData = {
+      rows: [
+        { id: 1, email: 'testuser@gmail.com', admin_password: 'testpass', first_name: 'Test', last_name: 'User'}
+      ]
+    };
+    const req = { body: { first_name: 'Test', last_name: 'User', email: 'testuser@gmail.com', admin_password: 'testpass' } };
+    const res = { locals: {} };
+    const next = jest.fn();
+
+    db.query = jest.fn().mockResolvedValue(mockAdminData);
+
+    // call createAdmin middleware
+    await createAdmin(req, res, next);
+    
+    expect(res.locals.newAdmin).toEqual(mockAdminData.rows[0]);
+    expect(next).toHaveBeenCalledTimes(1);
+    });
+
+  it('should call next() with an error message if the database query fails', async () => {
+    // mock the db query to throw an error
+    const mockError = new Error('Database connection error');
+    db.query.mockRejectedValue(mockError);
+
+    const req = { body: { first_name: 'Test', last_name: 'User', email: 'testuser@gmail.com', admin_password: 'testpass' } };
+    let res = { locals: {} };
+
+    // mock Jest function for error
+    const next = jest.fn();
+
+    // call createAdmin middleware
+    await createAdmin(req, res, next);
+
+    expect(next).toHaveBeenCalledWith({log: 'adminController.createAdmin: ERROR', message: {err: 'Error occurred in adminController.createAdmin'} } );
+  })
+});
+
+
+
+
--- server/Controllers/adminController/middleware/getAdminById.test.js
+++ server/Controllers/adminController/middleware/getAdminById.test.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const request = require('supertest');
 
 const db = require('../../../models/adminModel');
