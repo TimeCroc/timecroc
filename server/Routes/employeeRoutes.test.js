a4fdc770987cbe1d8873c3f1472d6680ca44ffc6
Mark Charles Smith <markcharlessmith@gmail.com>
2023-09-14 12:10:34 -0700

    rewrote tests to avoid mutating data
    Co-authored-by: CLARECERULLO <JEWELOFSONG@GMAIL.COM>

--- server/Routes/employeeRoutes.test.js
+++ server/Routes/employeeRoutes.test.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const request = require('supertest');
 const employeeRouter = require('./employeeRoutes');
+const db = require('../models/employeeModel');
 const http = require('http');
-
 const app = express();
 const server = http.createServer(app);
+
+// mock the employeeModel
+jest.mock('../models/employeeModel', () => ({
+  query: jest.fn()
+}));
 
 app.use(express.json());
 // mount the employeeRouter on the app
 app.use('/api/employees', employeeRouter);
 
 describe('Employee Routes', () => {
+  // Clear all mocks before each test
+  beforeEach(() => {
+    console.log('before the test');
+    jest.clearAllMocks();
+  });
 
+  // Close the server after each test
   afterEach((done) => {
     console.log('after the test');
     server.close(() => {
@@ -22,41 +33,48 @@ describe('Employee Routes', () => {
   // Testing route to get all employees
   describe('/api/employees', () => {
     describe('GET', () => {
-      it('responds with a 200 status and return all employees', (done) => {
-        request(app)
+      it('responds with a 200 status and return all employees', async () => {
+        // Define mock data for the database query
+        const mockEmployeesData = [
+            { id: 1, pin: '1234', first_name: 'Ladybug', last_name: 'Critter', phone: '1235555555', email: 'ladybugisawesome@gmail.com', hourly_rate: 99},
+            { id: 2, pin: '4321', first_name: 'Mark', last_name: 'Yencheske', phone: '1235995599', email: 'markrulez@gmail.com', hourly_rate: 10}
+        ];
+
+        // Mock the behavior of db.query when getting all employees
+        db.query.mockResolvedValue({ rows: mockEmployeesData });
+
+        // Send a GET request to get all employees
+        const response = await request(app)
           .get('/api/employees')
           .expect(200)
           .expect('Content-Type', /application\/json/)
-          .end((err, res) => {
-            if(err) return done(err);
+   
+          // Verify the response
+          expect(response.status).toBe(200);
+          expect(response.headers['content-type']).toMatch(/application\/json/);
+          expect(response.body).toEqual(mockEmployeesData);
 
-            expect(res.status).toBe(200);
-            expect(res.headers['content-type']).toMatch(/application\/json/);
-            
-            done();
-          });
+        });
       });
     });
-  });
 
   // Testing route to get one employee
   describe('/api/employees/:pin', () => {
     describe('GET', () => {
-      it('responds with a 200 status and returns one employee', (done) => {
+      it('responds with a 200 status and returns one employee', async () => {
         const mockEmployeePin = 1234;
+        const mockEmployeeData = { id: 1, pin: '1234', first_name: 'Ladybug', last_name: 'Critter', phone: '1235555555', email: 'ladybugisawesome@gmail.com', hourly_rate: 99};
+
+        db.query.mockResolvedValue({ rows: [mockEmployeeData] });
       
-        request(app)
+        const response = await request(app)
           .get(`/api/employees/${mockEmployeePin}`)
           .expect(200)
           .expect('Content-Type', /application\/json/)
-          .end((err, res) => {
-            if(err) return done(err);
 
-            expect(res.status).toBe(200);
-            expect(res.headers['content-type']).toMatch(/application\/json/);
-            
-            done();
-          });
+          expect(response.status).toBe(200);
+          expect(response.headers['content-type']).toMatch(/application\/json/);
+          expect(response.body).toEqual(mockEmployeeData);
       });
     });
   });
@@ -64,8 +82,23 @@ describe('Employee Routes', () => {
   // Testing route to create new employee
   describe('/api/employees', () => {
     describe('POST', () => {
-      it('responds with a 200 status and creates new employee', (done) => {
-        request(app)
+      it('responds with a 200 status and creates a new employee', async () => {
+        // Define mock data for the database query
+        const mockNewEmployeeData = {
+          id: 123,
+          pin: '9090',
+          first_name: 'Winnie',
+          last_name: 'ThePooh',
+          phone: '2345555555',
+          email: 'poohbear@gmail.com',
+          hourly_rate: 15,
+        };
+
+        // Mock the behavior of db.query when creating a new employee
+        db.query.mockResolvedValue({ rows: [mockNewEmployeeData] });
+
+        // Send a POST request to create a new employee
+        const response = await request(app)
           .post('/api/employees')
           .send({
             pin: '9090',
@@ -73,20 +106,55 @@ describe('Employee Routes', () => {
             last_name: 'ThePooh',
             phone: '2345555555',
             email: 'poohbear@gmail.com',
-            hourly_rate: 15
+            hourly_rate: 15,
           })
           .expect('Content-Type', /application\/json/)
-          .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
+          .expect(200);
 
-            expect(res.status).toBe(200);
-            expect(res.headers['content-type']).toMatch(/application\/json/);
+        // Verify the response
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toMatch(/application\/json/);
+        expect(response.body).toEqual(mockNewEmployeeData);
+      });
+    });
+  });
 
-            done();
+
+  // Testing route to update employee
+  describe('/api/employees/:pin', () => {
+    describe('PUT', () => {
+      it('responds with a 200 status and updates employee', async () => {
+        const mockEmployeePin = 9805;
+        const mockUpdatedEmployeeData = {
+          // id: 123,
+          pin: '9805',
+          first_name: 'Winnifredson',
+          last_name: 'ThePooooooh',
+          phone: '9876543210',
+          email: 'poohisawesome@gmail.com',
+          hourly_rate: 20
+        };
+
+        db.query.mockResolvedValue({ rows: [mockUpdatedEmployeeData] });
+
+        const response = await request(app)
+          .put(`/api/employees/${mockEmployeePin}`)
+          .send({
+            pin: '9805',
+            first_name: 'Winnifredson',
+            last_name: 'ThePooooooh',
+            phone: '9876543210',
+            email: 'poohisawesome@gmail.com',
+            hourly_rate: 20
           })
+          .expect('Content-Type', /application\/json/)
+          .expect(200);
+
+          expect(response.status).toBe(200);
+          expect(response.headers['content-type']).toMatch(/application\/json/);
+          expect(response.body).toEqual(mockUpdatedEmployeeData);
+          })
+        })
       })
     })
-  })
-  
-});
+// });
