23f0906166404025751d6aa23e51783f6c5ce34a
Mark Charles Smith <markcharlessmith@gmail.com>
2023-09-19 10:33:02 -0700

    added test for starting a shift to shiftRoutes.test.js
    Co-authored-by: CLARECERULLO <JEWELOFSONG@GMAIL.COM>

--- server/Routes/shiftRoutes.test.js
+++ server/Routes/shiftRoutes.test.js
@@ -46,7 +46,7 @@ describe('Shift Routes', () => {
               { _id: 123, employee_id: 1, shift_date: '2020-01-01', start_time: 1577836800000, end_time: 1577869200000, tips: 108, reimbursements: 200, tours: 3, doc: 1},
               { _id: 456, employee_id: 1, shift_date: '2020-01-02', start_time: 1577932800000, end_time: 1577965200000, tips: 108, reimbursements: 100, tours: 2, doc: 2}
             ];
-            
+
             employeeController.getOneEmployee.mockImplementation((req, res, next) => {
                 res.locals.targetEmployee = mockEmployeeData;
                 return next();
@@ -115,6 +115,34 @@ describe('Shift Routes', () => {
       })
 
   // Testing route to start a shift
+  describe('/api/shifts/:pin', () => {
+    describe('POST', () => {
+        it('responds with a 200 status and creates a new shift', async () => {
+            const mockEmployeePin = 9090;
+            const mockEmployeeData = { id: 2, pin: '9090', first_name: 'Ladybug', last_name: 'Critter', phone: '1235555555', email: 'ladybugisawesome@gmail.com', hourly_rate: 99};
+            const mockLoginShiftData = { _id: 500, employee_id: 2, shift_date: '2023-05-28', start_time: 1672251499999 }
+
+            employeeController.getOneEmployee.mockImplementation((req, res, next) => {
+                res.locals.targetEmployee = mockEmployeeData;
+                return next();
+            })
+            shiftController.login.mockImplementation((req, res, next) => {
+                res.locals.newShift = mockLoginShiftData;
+                return next();
+            })
+
+            const response = await request(app)
+                .post(`/api/shifts/${mockEmployeePin}`)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+
+            expect(response.status).toBe(200);
+            expect(response.headers['content-type']).toMatch(/application\/json/);
+            expect(response.body).toEqual(mockLoginShiftData);
+        })
+      })
+    })
   // Testing route to end a shift (update)
+  
   // Testing route to update extras
 });
