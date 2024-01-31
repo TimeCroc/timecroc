2a52709b37b1f975094e3e622abe535712e566d8
Mark Yencheske <71556587+markyencheske@users.noreply.github.com>
2024-01-31 09:48:31 -0600

    updated employee list and started edit shift component

---
+++ client/components/admin/EditShift/EditShift.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './EditShift.css'
+
+const EditShift = (props) => {
+
+  return (
+    <div className='edit-shift-container'>
+      <h4>Edit Shift Component</h4>
+    </div>
+  );
+}
